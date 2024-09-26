b326f869b13e45d9f4f560ee0f37a916ce97a628
sanjanahariyani <mcasanjana05@gmail.com>
2024-09-26 09:10:42 +0530

    Update ex.sql

--- sql_ex3/ex.sql
+++ sql_ex3/ex.sql
@@ -1,10 +1,10 @@
  create table voters
-  2  (
-  3  id number(10)primary key,
-  4  name varchar2(10) not null,
-  5  state varchar2(10)check(state in('gujarat','maharashtra')),
-  6  city varchar2(10)check(city in('rajkot','gandhinagar','ahemdabad','mumbai','pune','nagpur'))
-  7  );
+  (
+   id number(10)primary key,
+   name varchar2(10) not null,
+   state varchar2(10)check(state in('gujarat','maharashtra')),
+   city varchar2(10)check(city in('rajkot','gandhinagar','ahemdabad','mumbai','pune','nagpur'))
+   );
 
 
  1... alter table voters add ward_no number(5);
@@ -37,9 +37,11 @@ SQL> desc voters;
 
 
  3....
- SQL> alter table voters modify id varchar2(10) check(id like 'E____');
+ SQL> ALTER TABLE voters MODIFY id VARCHAR2(10);
 
 Table altered.
+ 
+ ALTER TABLE voters ADD CONSTRAINT chk_voters_id CHECK (voters_id LIKE 'E____');
 
 SQL> desc voters;
  Name                                      Null?    Type
@@ -82,11 +84,11 @@ SQL> desc voters;
  GENDER                                             VARCHAR2(10)
 
  6..
-  insert into voters values('E1211','aaa','gujarat','rajkot',111,'female');
- insert into voters values('E1212','bbb','gujarat','rajkot',222,'female');
- insert into voters values('E1213','ccc','gujarat','pune',333,'female');
- insert into voters values('E1214','ddd','gujarat','nagpur',444,'male');
- insert into voters values('E1215','eee','gujarat','rajkot',555,'male');
+  insert into voters values('E1211','aaa','gujarat','rajkot',11,'female');
+ insert into voters values('E1212','bbb','gujarat','rajkot',12,'female');
+ insert into voters values('E1213','ccc','gujarat','pune',13,'female');
+ insert into voters values('E1214','ddd','gujarat','nagpur',14,'male');
+ insert into voters values('E1215','eee','gujarat','rajkot',15,'male');
 
 
 
@@ -176,57 +178,8 @@ E1215      eee        gujarat              rajkot            555 male           
 
 13...
 
+alter table voters drop constraint chk_voters_id;
 
-SQL> alter table voters drop constraint where voters_id='E____';
-alter table voters drop constraint where voters_id='E____'
-                                   *
-ERROR at line 1:
-ORA-02250: missing or invalid constraint name
-
-
-SQL> alter table voters drop where voters_id='E____';
-alter table voters drop where voters_id='E____'
-                        *
-ERROR at line 1:
-ORA-00905: missing keyword
-
-
-SQL> alter table voters drop voters_id='E____';
-alter table voters drop voters_id='E____'
-                        *
-ERROR at line 1:
-ORA-00905: missing keyword
-
-
-SQL> alter table voters drop constraint;
-alter table voters drop constraint
-                                 *
-ERROR at line 1:
-ORA-02250: missing or invalid constraint name
-
-
-SQL> alter table voters drop column voters_id;
-
-Table altered.
-
-SQL> alter table voters voters_id varchar2(20) constraint aa check(voters_id like 'E____');
-alter table voters voters_id varchar2(20) constraint aa check(voters_id like 'E____')
-                   *
-ERROR at line 1:
-ORA-01735: invalid ALTER TABLE option
-
-
-SQL> select * from voters;
-
-NAME       STATE                CITY          WARD_NO GENDER            AGE
----------- -------------------- ---------- ---------- ---------- ----------
-aaa        gujarat              rajkot            111 female             22
-bbb        gujarat              rajkot            222 female             19
-ccc        maharashtra          pune               16 female             22
-ddd        gujarat              nagpur            444 male               23
-eee        gujarat              rajkot            555 male               20
-
-SQL>
 
 
 
